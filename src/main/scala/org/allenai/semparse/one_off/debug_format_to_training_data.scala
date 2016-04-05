7614bd4a5abb1102360087af01ed2b6f4bca8201
Matt Gardner <mattg@allenai.org>
2016-04-05 20:07:11 +0000

    Got the science sentence processor working with spark
    Former-commit-id: 9b641215de248cef17dc5d441b31ab7ebbb081ef
    Former-commit-id: 7ec00dc890606c2bf19df0e2d802e353b70a730b [formerly 2cbee81a31ff0eb85285f4371d93f341055a4e08]
    Former-commit-id: 131210e55e87b8cda8e1e0ecb541d14aeee6a6dd

---
+++ src/main/scala/org/allenai/semparse/one_off/debug_format_to_training_data.scala
@@ -0,0 +1,39 @@
+package org.allenai.semparse.one_off
+
+import com.mattg.util.FileUtil
+
+import org.allenai.semparse.parse.Predicate
+import org.allenai.semparse.pipeline.Helper
+
+object debug_format_to_training_data {
+  val inputFile = "/home/mattg/clone/semparse/data/science_sentences.txt"
+  val outputFile = "/home/mattg/clone/semparse/data/science_sentences_training_data.txt"
+  val fileUtil = new FileUtil
+  def main(args: Array[String]) {
+    val lines = fileUtil.readLinesFromFile(inputFile).par
+    val parsed = lines.map(parseDebugLine)
+    val outputLines = parsed.flatMap(Helper.logicalFormToTrainingData)
+    fileUtil.writeLinesToFile(outputFile, outputLines.seq)
+  }
+
+  def parseDebugLine(line: String): (String, Set[Predicate]) = {
+    val fields = line.split(" -> ")
+    val sentence = fields(0)
+    if (fields.length == 1) return (sentence, Set())
+    val lf = fields(1)
+    val lf_fields = lf.split("\\) ")
+    val predicates = lf_fields.map(lf_field => {
+      val paren = lf_field.indexOf("(")
+      val predicate = lf_field.substring(0, paren)
+      val args = lf_field.substring(paren + 1).split(", ").map(arg => {
+        if (arg.endsWith(")")) {
+          arg.substring(0, arg.length - 1)
+        } else {
+          arg
+        }
+      })
+      Predicate(predicate, args.toSeq)
+    }).toSet
+    (sentence, predicates)
+  }
+}
