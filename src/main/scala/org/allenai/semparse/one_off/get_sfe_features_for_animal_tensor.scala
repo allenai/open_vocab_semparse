3c7a63adb4ab9038eff2e73d1321f96394d35921
Matt Gardner <mattg@allenai.org>
2016-05-26 01:09:07 +0000

    Improvements to the feature pipeline
    Though this should still be moved somewhere else...  Probably to the PRA code.  The only feature
    selection code I have currently lives in this repo, so this is here for now, until I get time for
    code clean up after paper submissions...
    
    
    Former-commit-id: ffe41e282392b53f4226f7348a484a6f25683e12
    Former-commit-id: b5e295552eae97bcdcece1254b10d136e69102ec [formerly 5cdab0670bb5fb78c8ead418341a1699777c11ef] [formerly 9b45fdfe909e6bb70c02826e03301b13ce32bcad [formerly 2bd38d3276c940af1b76377b5214c599d767def6]] [formerly ae80e05f38435eecbe948e2885a76a5c869b84f0 [formerly d8a8fbba19e113a000b24bc0682ad276c3de1a2c] [formerly a68783847ab74e6384a00bf7fc1d0fa8bbe12e44 [formerly d44401160613c7f816de210811718fda1466504d]]]
    Former-commit-id: 9b3cb7fe4a529216bf79bb6f94cca21bcfc3c58a [formerly 5e60c216426c0e0991ce4d98f702c27d09eca345] [formerly ca598011916d22686271c652c327123ed2bd38fb [formerly 7220daa45ea2390cf0f44f0591f7c547a2ce5c59]]
    Former-commit-id: eebbbb34a8cc0fca0e53b9d1dba8d5765136e0cb [formerly b807dd6300911b3732f1db1b2a77c23444cef48f]
    Former-commit-id: 5b2bce88be3b74284d7104ddabf701eabfb8f573

--- src/main/scala/org/allenai/semparse/one_off/get_sfe_features_for_animal_tensor.scala
+++ src/main/scala/org/allenai/semparse/one_off/get_sfe_features_for_animal_tensor.scala
@@ -1,23 +1,29 @@
 package org.allenai.semparse.one_off
 
+import org.allenai.semparse.pipeline.base.PreFilteredFeatureComputer
 import org.allenai.semparse.pipeline.base.SparkPmiComputer
 import org.allenai.semparse.pipeline.base.TrainingDataFeatureComputer
 
 import com.mattg.util.Dictionary
+import com.mattg.util.FileUtil
 import com.mattg.util.ImmutableDictionary
 import com.mattg.util.MutableConcurrentDictionary
-import com.mattg.util.FileUtil
+import com.mattg.util.SpecFileReader
 
 import org.json4s._
 import org.json4s.JsonDSL._
 
+import java.io.FileWriter
+
 import scala.collection.mutable
 
 object get_sfe_features_for_animal_tensor {
+  implicit val formats = DefaultFormats
+  val fileUtil = new FileUtil
 
   // Input files
   val sfeSpecFile = "/home/mattg/data/animal_tensor_kbc/sfe_spec.json"
-  val nodePairFile = "/home/mattg/data/animal_tensor_kbc/node_pairs.tsv"
+  val nodePairFile = "/home/mattg/data/animal_tensor_kbc/all_node_pairs.tsv"
   val nodePairRelationFile = "/home/mattg/data/animal_tensor_kbc/node_pair_relations.tsv"
   val entityDictFile = "/home/mattg/data/animal_tensor_kbc/entity_dict.tsv"
   val relationDictFile = "/home/mattg/data/animal_tensor_kbc/relation_dict.tsv"
@@ -43,35 +49,130 @@ object get_sfe_features_for_animal_tensor {
   val featureSeparator = " -#- "
 
   def main(args: Array[String]) {
-    val fileUtil = new FileUtil
     val pmiComputer = new SparkPmiComputer(pmiComputerParams, fileUtil)
-    //pmiComputer.runPipeline()
+    /*
+    pmiComputer.runPipeline()
+    convertFeatureFileToTensorFormat(
+      pmiComputer.relWordFeatureFile,
+      pmiComputer.filteredMidPairFeatureFile,
+      finalFeatureFile,
+      fileUtil
+    )
+    */
+    getFeaturesForNodePairs(
+      nodePairFile,
+      sfeSpecFile,
+      pmiComputer.relWordFeatureFile,
+      finalFeatureFile
+    )
+  }
+
+  def writeNodePairFeaturesToTensorFile(
+    source: String,
+    target: String,
+    features: Seq[String],
+    entityDict: Dictionary,
+    relationDict: Dictionary,
+    relationFeatures: Map[String, Set[String]],
+    relationFeatureDictionaries: mutable.HashMap[String, Dictionary],
+    writer: FileWriter
+  ) {
+    val sourceIndex = entityDict.getIndex(source)
+    val targetIndex = entityDict.getIndex(target)
+    for (relation <- relationFeatures.keys) {
+      val relationIndex = relationDict.getIndex(relation)
+      val featureDict = relationFeatureDictionaries.getOrElseUpdate(relation, new MutableConcurrentDictionary)
+      for (feature <- features) {
+        if (relationFeatures(relation).contains(feature)) {
+          val featureIndex = featureDict.getIndex(feature) - 1
+          writer.write(s"$sourceIndex\t$targetIndex\t$relationIndex\t$featureIndex\n")
+        }
+      }
+    }
+  }
+
+  def getFeaturesForNodePairs(
+    nodePairFile: String,
+    specFile: String,
+    relationFeatureFile: String,
+    outputFile: String
+  ) {
+    val sfeParams = new SpecFileReader("/dev/null").readSpecFile(specFile)
+    val graphDirectory = (sfeParams \ "graph").extract[String]
+    val computer = new PreFilteredFeatureComputer(sfeParams, fileUtil)
+    val writer = fileUtil.getFileWriter(outputFile)
     val entityDict = ImmutableDictionary.readFromFile(entityDictFile)
     val relationDict = ImmutableDictionary.readFromFile(relationDictFile)
     val relationFeatureDictionaries = new mutable.HashMap[String, Dictionary]
-    val relationFeatures = fileUtil.mapLinesFromFile(pmiComputer.relWordFeatureFile, (line) => {
+    val relationFeatures = loadRelationFeatureFiles(relationFeatureFile)
+    val allAllowedFeatures = relationFeatures.flatMap(_._2).toSet
+    val nodePairs = fileUtil.mapLinesFromFile(nodePairFile, (line) => {
+      val fields = line.split(entitySeparator)
+      (entityDict.getString(fields(0).toInt), entityDict.getString(fields(1).toInt))
+    })
+    var done = 0
+    var chunkSize = 1024
+    nodePairs.grouped(chunkSize).foreach(nodePairGroup => {
+      val nodePairFeatures = nodePairGroup.par.map(nodePair => {
+        val (source, target) = nodePair
+        val features = computer.computeMidPairFeatures(source, target).filter(f => allAllowedFeatures.contains(f))
+        (source, target, features)
+      }).seq
+      for ((source, target, features) <- nodePairFeatures) {
+        writeNodePairFeaturesToTensorFile(
+          source,
+          target,
+          features,
+          entityDict,
+          relationDict,
+          relationFeatures,
+          relationFeatureDictionaries,
+          writer
+        )
+      }
+      done += chunkSize
+      println(s"Done with $done")
+    })
+    writer.close()
+    fileUtil.mkdirs(featureDictionaryDir)
+    for ((relation, dictionary) <- relationFeatureDictionaries) {
+      dictionary.writeToFile(featureDictionaryDir + relation)
+    }
+  }
+
+  def loadRelationFeatureFiles(filename: String) = {
+    fileUtil.mapLinesFromFile(filename, (line) => {
       val fields = line.split("\t")
       val relation = fields(0)
       val features = fields.drop(1)
       (relation -> features.toSet)
     }).toMap
-    val writer = fileUtil.getFileWriter(finalFeatureFile)
-    fileUtil.processFile(pmiComputer.filteredMidPairFeatureFile, (line: String) => {
+  }
+
+  def convertFeatureFileToTensorFormat(
+    relationFeatureFile: String,
+    nodePairFeatureFile: String,
+    outputTensorFile: String
+  ) {
+    val entityDict = ImmutableDictionary.readFromFile(entityDictFile)
+    val relationDict = ImmutableDictionary.readFromFile(relationDictFile)
+    val relationFeatureDictionaries = new mutable.HashMap[String, Dictionary]
+    val relationFeatures = loadRelationFeatureFiles(relationFeatureFile)
+    val writer = fileUtil.getFileWriter(outputTensorFile)
+    fileUtil.processFile(nodePairFeatureFile, (line: String) => {
       val fields = line.split("\t")
       val nodePair = fields(0).split(entitySeparator)
       val features = fields(1).split(featureSeparator)
-      val sourceIndex = entityDict.getIndex(nodePair(0))
-      val targetIndex = entityDict.getIndex(nodePair(1))
-      for (relation <- relationFeatures.keys) {
-        val relationIndex = relationDict.getIndex(relation)
-        val featureDict = relationFeatureDictionaries.getOrElseUpdate(relation, new MutableConcurrentDictionary)
-        for (feature <- features) {
-          if (relationFeatures(relation).contains(feature)) {
-            val featureIndex = featureDict.getIndex(feature) - 1
-            writer.write(s"$sourceIndex\t$targetIndex\t$relationIndex\t$featureIndex\n")
-          }
-        }
-      }
+      writeNodePairFeaturesToTensorFile(
+        nodePair(0),
+        nodePair(1),
+        features,
+        entityDict,
+        relationDict,
+        relationFeatures,
+        relationFeatureDictionaries,
+        writer
+      )
     })
     writer.close()
     fileUtil.mkdirs(featureDictionaryDir)
