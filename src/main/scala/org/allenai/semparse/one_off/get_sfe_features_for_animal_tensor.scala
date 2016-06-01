a177016454cee72cb10d83f82e5846ac18b3c168
Matt Gardner <mattg@allenai.org>
2016-06-01 22:54:58 +0000

    Some improvements to getting SFE features for the animal tensor experiments
    I still need to move much of this code to the SFE codebase, I think.
    
    
    Former-commit-id: 6273b19ba79d2cfb17e7804b67b7fd797517794a
    Former-commit-id: 5718f360810987915be9db4d5a0ac0f0b7d703dd [formerly 21a31cf1d99e4a39f32efc659c4a1fc4294103b1] [formerly 58186b59594947ee76231d6ce5b9341f95d7c4f7 [formerly fbc5b5517e17989b6963999dc56b687f13e3acea]] [formerly 6fd9eba8be85881e8abc0aab47b9d9c52ef0e686 [formerly 580b3bd9228ad33307fc5d38ec2aae4dbbc6d198] [formerly fc11641db1f29f18543328487ad52824f5f64312 [formerly 5378b60c971408e9dad739e4511c64b0044d2421]]]
    Former-commit-id: f103d1aaec4393e06aba878bbc3410674d413774 [formerly 4c722ec3ea45f5a1aaada23f2c9f02b58d238f2d] [formerly 1313dab74803ca0892763d0bbc5379718a59dae2 [formerly f1f326e4e4bf1ce61abca4893acd371b5e93f75e]]
    Former-commit-id: 81a713f41793309d96ff73a3d74746138b161553 [formerly 32c308e2af253a05b8b5387e02a7eaa99f2a6826]
    Former-commit-id: ecfed7dbfe725bd1974c972ccfc96f28c02c3b8e

--- src/main/scala/org/allenai/semparse/one_off/get_sfe_features_for_animal_tensor.scala
+++ src/main/scala/org/allenai/semparse/one_off/get_sfe_features_for_animal_tensor.scala
@@ -10,12 +10,23 @@ import com.mattg.util.ImmutableDictionary
 import com.mattg.util.MutableConcurrentDictionary
 import com.mattg.util.SpecFileReader
 
+import edu.cmu.ml.rtw.pra.data.NodePairInstance
+import edu.cmu.ml.rtw.pra.data.NodePairSplit
+import edu.cmu.ml.rtw.pra.experiments.Outputter
+import edu.cmu.ml.rtw.pra.experiments.RelationMetadata
+import edu.cmu.ml.rtw.pra.features.FeatureMatrix
+import edu.cmu.ml.rtw.pra.features.NodePairSubgraphFeatureGenerator
+import edu.cmu.ml.rtw.pra.graphs.Graph
+import edu.cmu.ml.rtw.pra.models.LogisticRegressionModel
+import edu.cmu.ml.rtw.pra.operations.HackyHanieOperation
+
 import org.json4s._
 import org.json4s.JsonDSL._
 
 import java.io.FileWriter
 
 import scala.collection.mutable
+import scala.collection.JavaConverters._
 
 object get_sfe_features_for_animal_tensor {
   implicit val formats = DefaultFormats
@@ -65,6 +76,18 @@ object get_sfe_features_for_animal_tensor {
       pmiComputer.relWordFeatureFile,
       finalFeatureFile
     )
+    /*
+    val splitDir = "/home/mattg/pra/splits/animals_with_negatives/"
+    val relationToRun = "eat"
+    val outFile = "eat_scores.tsv"
+    testSfeWithSelectedFeatures(
+      pmiComputer.relWordFeatureFile,
+      sfeSpecFile,
+      splitDir,
+      relationToRun,
+      outFile
+    )
+    */
   }
 
   def writeNodePairFeaturesToTensorFile(
@@ -98,9 +121,18 @@ object get_sfe_features_for_animal_tensor {
     outputFile: String
   ) {
     val sfeParams = new SpecFileReader("/dev/null").readSpecFile(specFile)
-    val graphDirectory = (sfeParams \ "graph").extract[String]
-    val computer = new PreFilteredFeatureComputer(sfeParams, fileUtil)
     val writer = fileUtil.getFileWriter(outputFile)
+    val outputterParams: JValue = ("outdir" -> "tmp_output/")
+    val outputter = new Outputter(outputterParams, "/dev/null", "ignored", fileUtil)
+    val relationMetadata = new RelationMetadata(sfeParams \ "relation metadata", "/dev/null", outputter, fileUtil)
+    val graph = Graph.create(sfeParams \ "graph", "", outputter, fileUtil).get
+    val featureGenerator = new NodePairSubgraphFeatureGenerator(
+      sfeParams \ "node pair features",
+      "ignored relation",
+      relationMetadata,
+      outputter,
+      fileUtil = fileUtil
+    )
     val entityDict = ImmutableDictionary.readFromFile(entityDictFile)
     val relationDict = ImmutableDictionary.readFromFile(relationDictFile)
     val relationFeatureDictionaries = new mutable.HashMap[String, Dictionary]
@@ -113,22 +145,33 @@ object get_sfe_features_for_animal_tensor {
     var done = 0
     var chunkSize = 1024
     nodePairs.grouped(chunkSize).foreach(nodePairGroup => {
-      val nodePairFeatures = nodePairGroup.par.map(nodePair => {
+      val subgraphs = nodePairGroup.par.flatMap(nodePair => {
         val (source, target) = nodePair
-        val features = computer.computeMidPairFeatures(source, target).filter(f => allAllowedFeatures.contains(f))
-        (source, target, features)
-      }).seq
-      for ((source, target, features) <- nodePairFeatures) {
-        writeNodePairFeaturesToTensorFile(
-          source,
-          target,
-          features,
-          entityDict,
-          relationDict,
-          relationFeatures,
-          relationFeatureDictionaries,
-          writer
-        )
+        if (!graph.hasNode(source) || !graph.hasNode(target)) {
+          Seq()
+        } else {
+          val instance = new NodePairInstance(graph.getNodeIndex(source), graph.getNodeIndex(target), true, graph)
+          val subgraph = featureGenerator.getLocalSubgraph(instance)
+          Seq((instance, subgraph))
+        }
+      })
+      for ((instance, subgraph) <- subgraphs) {
+        for (relation <- relationFeatures.keys) {
+          val filteredSubgraph = featureGenerator.filterSubgraph(instance, subgraph, relation)
+          val features = featureGenerator.extractFeaturesAsStrings(instance, filteredSubgraph)
+          val sourceIndex = entityDict.getIndex(graph.getNodeName(instance.source))
+          val targetIndex = entityDict.getIndex(graph.getNodeName(instance.target))
+          val relationIndex = relationDict.getIndex(relation)
+          val featureDict = relationFeatureDictionaries.getOrElseUpdate(relation, new MutableConcurrentDictionary)
+          for (feature <- features) {
+            if (relationFeatures(relation).contains(feature)) {
+              val featureIndex = featureDict.getIndex(feature) - 1
+              writer synchronized {
+                writer.write(s"$sourceIndex\t$targetIndex\t$relationIndex\t$featureIndex\n")
+              }
+            }
+          }
+        }
       }
       done += chunkSize
       println(s"Done with $done")
@@ -179,5 +222,74 @@ object get_sfe_features_for_animal_tensor {
     for ((relation, dictionary) <- relationFeatureDictionaries) {
       dictionary.writeToFile(featureDictionaryDir + relation)
     }
+  }
+
+  def testSfeWithSelectedFeatures(
+    relationFeatureFile: String,
+    specFile: String,
+    splitDir: String,
+    relationToRun: String,
+    outfile: String
+  ) {
+    val relationFeatures = loadRelationFeatureFiles(relationFeatureFile)
+    val allowedFeatures = relationFeatures(relationToRun)
+
+    // SFE set up code
+    val sfeParams = new SpecFileReader("/dev/null").readSpecFile(specFile)
+    val outputterParams: JValue = ("outdir" -> "tmp_output/")
+    val outputter = new Outputter(outputterParams, "/dev/null", "ignored", fileUtil)
+    outputter.setRelation(relationToRun)
+    val relationMetadata = new RelationMetadata(sfeParams \ "relation metadata", "/dev/null", outputter, fileUtil)
+    val graph = Graph.create(sfeParams \ "graph", "", outputter, fileUtil).get
+    val featureGenerator = new NodePairSubgraphFeatureGenerator(
+      sfeParams \ "node pair features",
+      relationToRun,
+      relationMetadata,
+      outputter,
+      fileUtil = fileUtil
+    )
+    val splitParams = JString(splitDir)
+    val split = new NodePairSplit(splitParams, "/dev/null", outputter, fileUtil)
+    val modelParams = ("l1 weight" -> 0.005) ~ ("l2 weight" -> 1)
+    val model = new LogisticRegressionModel[NodePairInstance](modelParams, outputter)
+    val operationParams = ("features" -> (sfeParams \ "node pair features"))
+    val operation = new HackyHanieOperation(
+      operationParams,
+      Some(graph),
+      split,
+      relationMetadata,
+      outputter,
+      fileUtil
+    )
+
+    println("Loading training and test data")
+    val trainingData = split.getTrainingData(relationToRun, Some(graph))
+    val testingData = split.getTestingData(relationToRun, Some(graph))
+
+    println("Creating training feature matrix")
+    val featureDict = new MutableConcurrentDictionary
+    val matrixRows = trainingData.instances.par.map(instance => {
+      val subgraph = featureGenerator.getLocalSubgraph(instance)
+      val features = featureGenerator.extractFeaturesAsStrings(instance, subgraph)
+      val keptFeatures = features.filter(allowedFeatures.contains).map(featureDict.getIndex)
+      featureGenerator.createMatrixRow(instance, keptFeatures)
+    }).seq
+
+    println("Training the model")
+    val featureNames = {
+      val features = (1 until featureDict.size).par.map(i => {
+          val name = featureDict.getString(i)
+          if (name == null) {
+            s"!!!!NULL FEATURE-${i}!!!!"
+          } else {
+            name
+          }
+      }).seq
+      ("!!!!NULL FEATURE!!!!" +: features).toArray
+    }
+    model.train(new FeatureMatrix(matrixRows.asJava), trainingData, featureNames)
+
+    println("Evaluating the model")
+    operation.runRelationWithModel(relationToRun, model, featureDict)
   }
 }
